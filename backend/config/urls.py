d01acfd2a8c9496a4a35fe9ff9d99e0bc99be582
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-06 00:20:57 +0300

    feat: include users urls to config.urls and configure auth/ handlers

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.users.views import LoginView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Admin
+    path("admin/", admin.site.urls),
+    # Auth
+    path("api/v1/auth/token/", LoginView.as_view(), name="token_obtain"),
+    path("api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Apps
+    path("api/v1/", include("apps.users.urls")),
 ]
