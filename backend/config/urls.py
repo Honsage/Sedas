396d9f44e5fad146bbd3d7b5ab4eb1919b407eab
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-06 17:29:50 +0300

    build: configure media directory and dev url for static files

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -13,4 +15,10 @@ urlpatterns = [
     path("api/v1/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # Apps
     path("api/v1/", include("apps.users.urls")),
+    path("api/v1/", include("apps.documents.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
