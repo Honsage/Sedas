9250afde42522ead53c761536a70177bddc785af
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-07 16:59:50 +0300

    feat(documents): register models in admin panel

--- backend/apps/documents/admin.py
+++ backend/apps/documents/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Document, DocumentVersion
+
+
+class DocumentVersionInline(admin.TabularInline):
+    model = DocumentVersion
+    extra = 0
+    readonly_fields = ("version_number", "file_hash", "author", "created_at")
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("title", "status", "created_at")
+    list_filter = ("status",)
+    search_fields = ("title",)
+    readonly_fields = ("created_at",)
+    inlines = [DocumentVersionInline]
+
+
+@admin.register(DocumentVersion)
+class DocumentVersionAdmin(admin.ModelAdmin):
+    list_display = ("document", "version_number", "author", "created_at")
+    readonly_fields = ("file_hash", "created_at")
