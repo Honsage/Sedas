993ff3fb8ecfd8dac9078327b20b511f4f924588
Honsage <musatov.i.a@edu.mirea.ru>
2026-05-07 17:00:05 +0300

    feat(signatures): register models in admin panel

--- backend/apps/signatures/admin.py
+++ backend/apps/signatures/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Signature
+
+
+@admin.register(Signature)
+class SignatureAdmin(admin.ModelAdmin):
+    list_display = ("signer", "document_version", "algorithm", "signed_at")
+    list_filter = ("algorithm",)
+    readonly_fields = ("signature_blob", "signed_at")
